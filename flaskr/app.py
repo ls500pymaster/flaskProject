21920a5fe644ba95edfc2ecd20514bc3630f5472
pythonmaster <theone7877@gmail.com>
2023-01-12 21:49:45 +0200

    line separator added

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,1 +1,51 @@
-# from flask import Flask, request, render_template# from faker import Faker# import pandas as pd# import requests## fake = Faker()# app = Flask(__name__)### @app.route('/requirements/', methods=['GET'])# def requirements():#     with open('requirements.txt') as f:#         lines = f.readlines()#     return lines### @app.route('/generate_users/', methods=['GET'])# def generate_users(count=100):#     count = int(request.args.get("count", 100))#     new_list = []#     for _ in range(count):#         new_list.append(fake.name())#         new_list.append(fake.email())#     return render_template('usernames.html', new_list=new_list)### @app.route('/mean/')# def mean():#     read_csv = pd.read_csv('../hw.csv')#     height = round(read_csv["height"].mean() * 2.54)#     weight = round(read_csv["weight"].mean() * 0.45359237)#     return render_template(#         'cosmo.html',#         title="HW.CSV",#         height=height,#         weight=weight,#     )### @app.route('/space/')# def space():#     response = requests.get('http://api.open-notify.org/astros.json')#     response.raise_for_status()#     json_response = response.json()#     for key, value in json_response.items():#         print(value)#     return render_template(#         'cosmo.html',#         title="SPACE",#         cosmo=value,#     )
+# from flask import Flask, request, render_template
+# from faker import Faker
+# import pandas as pd
+# import requests
+#
+# fake = Faker()
+# app = Flask(__name__)
+#
+#
+# @app.route('/requirements/', methods=['GET'])
+# def requirements():
+#     with open('requirements.txt') as f:
+#         lines = f.readlines()
+#     return lines
+#
+#
+# @app.route('/generate_users/', methods=['GET'])
+# def generate_users(count=100):
+#     count = int(request.args.get("count", 100))
+#     new_list = []
+#     for _ in range(count):
+#         new_list.append(fake.name())
+#         new_list.append(fake.email())
+#     return render_template('usernames.html', new_list=new_list)
+#
+#
+# @app.route('/mean/')
+# def mean():
+#     read_csv = pd.read_csv('../hw.csv')
+#     height = round(read_csv["height"].mean() * 2.54)
+#     weight = round(read_csv["weight"].mean() * 0.45359237)
+#     return render_template(
+#         'cosmo.html',
+#         title="HW.CSV",
+#         height=height,
+#         weight=weight,
+#     )
+#
+#
+# @app.route('/space/')
+# def space():
+#     response = requests.get('http://api.open-notify.org/astros.json')
+#     response.raise_for_status()
+#     json_response = response.json()
+#     for key, value in json_response.items():
+#         print(value)
+#     return render_template(
+#         'cosmo.html',
+#         title="SPACE",
+#         cosmo=value,
+#     )
--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,1 +1,47 @@
-from flask import (    Blueprint, render_template)from flaskr.db import get_dbbp = Blueprint('blog', __name__)@bp.route('/names/')def names():    db = get_db()    rows = db.execute('SELECT COUNT (DISTINCT artist) FROM tracks').fetchall()    return render_template('blog/artist.html', rows=rows)@bp.route('/tracks/')def tracks():    db = get_db()    rows = db.execute('SELECT COUNT (title) FROM tracks').fetchall()    return render_template('blog/tracks.html', rows=rows)@bp.route('/tracks/<genre>/')def tracks_genre(genre):    db = get_db()    all_tracks = db.execute("SELECT * FROM tracks WHERE genre = ? COLLATE NOCASE", (genre,)).fetchall()    return render_template('blog/tracks_genre.html', all_tracks_genre=len(all_tracks))@bp.route('/tracks-sec/')def tracks_sec():    db = get_db()    rows = db.execute('SELECT title, length / 1000.0 from tracks').fetchall()    return render_template('blog/tracks_sec.html', rows=rows)@bp.route('/tracks-sec/statistics/')def tracks_statistics():    db = get_db()    total_length = db.execute('SELECT SUM(length) FROM tracks;').fetchall()    avg = db.execute('SELECT AVG (ROUND(length / 1000.0)) FROM tracks;').fetchall()    return render_template('blog/tracks_stats.html', total_length=total_length, avg=avg)
+from flask import (
+    Blueprint, render_template
+)
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+
+@bp.route('/names/')
+def names():
+    db = get_db()
+    rows = db.execute('SELECT COUNT (DISTINCT artist) FROM tracks').fetchall()
+    return render_template('blog/artist.html', rows=rows)
+
+
+@bp.route('/tracks/')
+def tracks():
+    db = get_db()
+    rows = db.execute('SELECT COUNT (title) FROM tracks').fetchall()
+    return render_template('blog/tracks.html', rows=rows)
+
+
+@bp.route('/tracks/<genre>/')
+def tracks_genre(genre):
+    db = get_db()
+    all_tracks = db.execute("SELECT * FROM tracks WHERE genre = ? COLLATE NOCASE", (genre,)).fetchall()
+    return render_template('blog/tracks_genre.html', all_tracks_genre=len(all_tracks))
+
+
+@bp.route('/tracks-sec/')
+def tracks_sec():
+    db = get_db()
+    rows = db.execute('SELECT title, length / 1000.0 from tracks').fetchall()
+    return render_template('blog/tracks_sec.html', rows=rows)
+
+
+@bp.route('/tracks-sec/statistics/')
+def tracks_statistics():
+    db = get_db()
+    total_length = db.execute('SELECT SUM(length) FROM tracks;').fetchall()
+    avg = db.execute('SELECT AVG (ROUND(length / 1000.0)) FROM tracks;').fetchall()
+    return render_template('blog/tracks_stats.html', total_length=total_length, avg=avg)
+
+
+
+
+
